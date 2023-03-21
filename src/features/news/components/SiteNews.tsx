fd6ae3b90b126f63386cd9b275486a79f622d5f8
yuta-sksn <sukekawa@thymefield.jp>
2023-03-21 12:12:29 +0900

    feat: ページネーション仮実装

--- src/features/news/components/SiteNews.tsx
+++ src/features/news/components/SiteNews.tsx
@@ -3,6 +3,8 @@ import useSyncSiteNewsList from '@/features/news/api/useSyncSiteNewsList';
 import { isErrorModalObj, swrErrorHandlingInComponent } from '@/utils/error';
 import { Content } from '@/features/news/types/content';
 import { CardList } from '@/components/elements/CardList/CardList';
+import Pagination from '@mui/material/Pagination';
+import classes from './SiteNews.module.scss'
 
 interface SiteNewsProps {
   limit?: number;
@@ -13,9 +15,20 @@ export const SiteNews = ({
   limit,
   noPagination,
 }: SiteNewsProps) => {
-  const dataLimit = limit ?? 10
-  let dataOffset = 0 
+  // 現在のページ
+  const [page, setPage] = React.useState(1)
 
+  // ページネーション変更イベント
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value)
+  }
+
+  // データの最大表示数 (取得件数)
+  const dataLimit = limit ?? 10
+
+  // データ何件目から取得するかの指定
+  let dataOffset = (page - 1) * dataLimit
+  // noPagination が true なら 0
   if (noPagination) {
     dataOffset = 0
   }
@@ -25,14 +38,41 @@ export const SiteNews = ({
   const errorHandling = swrErrorHandlingInComponent(error);
   if (errorHandling !== null && !isErrorModalObj(errorHandling)) return errorHandling;
 
+  // ページネーションを表示するか否か
+  let showPagination = noPagination ?? false
+  if (data) {
+    showPagination = !noPagination && data.totalCount > data.limit
+  }
+
+  // ページ総数
+  let pageCount = showPagination ?
+    Math.floor(
+      (data?.totalCount as number) / (data?.limit as number)
+    ) + ((data?.totalCount as number) % (data?.limit as number) > 0 ? 1 : 0) : 0
+
   return (
     <>
       <div>
-      {(() => {
-        if (typeof data?.contents !== 'undefined' && typeof error === 'undefined') {
-          return (<CardList contents={data.contents as Content[]} />)
-        }
-      })()}
+        {/* お知らせ・イベント一覧 */}
+        {(() => {
+          if (typeof data?.contents !== 'undefined' && typeof error === 'undefined') {
+            return (<CardList contents={data.contents as Content[]} offset={data.offset} />)
+          }
+        })()}
+        {/* ページネーション */}
+        {(() => {
+          if (showPagination) {
+            return (
+              <Pagination
+                page={page}
+                count={pageCount}
+                color="primary"
+                className={classes.customPagination}
+                onChange={handleChange}
+              />
+            )
+          }
+        })()}
       </div>
       {/* エラーモーダル */}
       {(() => {
@@ -44,3 +84,7 @@ export const SiteNews = ({
     </>
   )
 };
+function useState(arg0: number): [any, any] {
+  throw new Error('Function not implemented.');
+}
+
